0873023efedcaf5f1cc06401e0bbdef9abf3299b
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-03 17:21:10 +0500

    component tree, and now start adding content in header

---
+++ brogram-gym-workout-webapp/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header>
+          <h1 className="text-gradient">The Brogram</h1>
+          <p><strong>The 30 Simple Workout Program</strong></p>
+    </header>
+  )
+}
+
+export default Header
