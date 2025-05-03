84f233b37d9f012c905a82b02db07030cd9ff57a
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-03 17:38:50 +0500

    working on font-family

--- brogram-gym-workout-webapp/components/Footer.jsx
+++ brogram-gym-workout-webapp/components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer></footer>
-  )
-}
+    <footer>
+      <p>
+        Built by{" "}
+        <Link target="_blank" href={"https://musamueed.vercel.app/"}>
+          Muhammad Musa Mueed
+        </Link>{" "}
+        Styled with <strong>FantaCSS</strong>
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
