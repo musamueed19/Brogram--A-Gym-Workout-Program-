1d67aabffedd0c8b76920d31928db7a9109a5e1b
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-05-03 18:03:55 +0500

    now, completed Hero Section, part-2

--- brogram-gym-workout-webapp/components/Hero.jsx
+++ brogram-gym-workout-webapp/components/Hero.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <>
+      {/* What they will get - Outcomes */}
       <h5>Complete the training program if you want to ...</h5>
       <ol className="benefits-list">
         <li>Follow a simple program with guaranteed results</li>
         <li>Get fit, healthy, strong, and shredded</li>
-              <li>Learn more about gym, training, and technique</li>
-              <li>Become a lifetime gym bro ❤</li>
+        <li>Learn more about gym, training, and technique</li>
+        <li>Become a lifetime gym bro 👍🏻</li>
       </ol>
+
+      {/* How they will use this app to acheive this */}
+      <h3>The Rules</h3>
+      <p>
+        To complete this program you <b>MUST</b> follow these simple rules:
+      </p>
+      <ul className="rules-list">
+        <div className="rule-item">
+          <p>
+            <b>Rest</b>
+          </p>
+          <p>Ensure that you are taking rest days where necessary</p>
+        </div>
+        <div className="rule-item">
+          <p>
+            <b>Reps</b>
+          </p>
+          <p>Every rep is a pause rep following a <abbr title="2 seconds down - 2 seconds pause - 2 seconds up">2 - 2 - 2 tempo</abbr></p>
+        </div>
+        <div className="rule-item">
+          <p>
+            <b>Weight*</b>
+          </p>
+          <p>Select the maximum weight that allows you to complete the set with good form</p>
+        </div>
+          </ul>
+          
+          {/*  */}
+          <small>* The first and second set would be at 75% and 85% of you working weight used for last two sets.</small>
+
+          {/*  */}
+          <h3>The Training Plan</h3>
+          <p>This training plan uses a strucutre known as the <b>Bro Split</b>, and follow this rotation 🔽</p>
+          <p><b><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></b></p>
+          <p>Complete all of the Workouts below and track your progress along the way ✅</p>
     </>
   );
-}
+};
 
-export default Hero
+export default Hero;
